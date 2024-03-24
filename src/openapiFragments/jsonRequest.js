2c5300995c3891f62410ec400289360b92dfecfa
FrancoConci <franco1conci@gmail.com>
2024-03-24 18:18:27 +0100

    Add jsDoc to openapi fragment utilities.

--- src/openapiFragments/jsonRequest.js
+++ src/openapiFragments/jsonRequest.js
@@ -1,4 +1,21 @@
-export const jsonRequest = (path = '', description = 'operation request') => ({
+/**
+ * @description Utility to generate a json request fragment for the API's JSON schema.
+ * @param {string} path Path to use as `$ref` in the API's JSON schema.
+ * @param {string} [description] Request description
+ * @returns A json request fragment with the supplied path and description
+ * * `
+ * {
+    description,
+    content: {
+        'application/json': {
+            schema: {
+              $ref: path,
+            },
+        },
+    }
+ * `
+ */
+export const jsonRequest = (path, description = 'operation request') => ({
   description,
   content: {
     'application/json': {
@@ -9,7 +26,29 @@ export const jsonRequest = (path = '', description = 'operation request') => ({
   },
 });
 
-export const jsonRequestOneOf = (paths = [''], description = 'operation request') => ({
+/**
+ * @description Utility to generate a json request fragment for the API's JSON schema.
+ * @param {string[]} paths Paths to use as `$ref` oneOf options in the API's JSON schema.
+ * @param {string} [description] Request description
+ * @returns A json request fragment with the supplied description and paths as oneOf options
+ * `
+ * {
+    description,
+    content: {
+        'application/json': {
+            schema: {
+                oneOf: [
+                    {
+                        $ref: path,
+                    },
+                ]
+            },
+        },
+    }
+ * `
+ */
+
+export const jsonRequestOneOf = (paths, description = 'operation request') => ({
   description,
   content: {
     'application/json': {
