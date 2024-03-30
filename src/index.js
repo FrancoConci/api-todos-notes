1ada88c01dca4faab17d61368beb4a34b614ef9e
FrancoConci <franco1conci@gmail.com>
2024-03-30 15:17:13 +0100

    Generate types from JSON schemas in openapi component/schemas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
+import { createWriteStream, rmSync, writeFileSync } from 'fs';
 import { dump } from 'js-yaml';
-import { writeFileSync } from 'fs';
+import { compile } from 'json-schema-to-typescript';
 import { openapi } from './openapi.js';
 
+// Create Openapi yaml
 writeFileSync('./output.yml', dump(openapi, { noRefs: true }), { encoding: 'utf8' });
+
+// Recreate types index and set a writer to it
+const typesPath = './src/types/index.ts';
+rmSync(typesPath, { force: true });
+writeFileSync(typesPath, '', { encoding: 'utf8' });
+const writer = createWriteStream(typesPath);
+
+// Write types from JSON schema to types index
+Object.keys(openapi.components.schemas).forEach((key) => {
+  const schema = openapi.components.schemas[key];
+  compile(schema, key, { bannerComment: false }).then((ts) =>
+    writer.write(ts, (error) => {
+      if (error) {
+        writer.close();
+        writer.end();
+      }
+    })
+  );
+});
+
+// Close writer and end stream
+writer.on('finish', () => {
+  writer.close();
+  writer.end();
+});
